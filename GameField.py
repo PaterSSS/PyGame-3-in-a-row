aaafe10aedc08d4410bd50b4d2fd3cba9582dbee
PaterSSS <leo.chel@mail.ru>
2024-04-08 23:22:06 +0300

    improve moving empty cells algorithm

--- GameField.py
+++ GameField.py
@@ -5,9 +5,9 @@ from Cell import Cell
 
 
 class Game:
-    def __init__(self, rows: int = 0, columns: int = 0, pathToFile: str = None):
-        if pathToFile is not None:
-            params = Game.read_field_from_file(pathToFile)
+    def __init__(self, rows: int = 0, columns: int = 0, path_to_file: str = None):
+        if path_to_file is not None:
+            params = Game.read_field_from_file(path_to_file)
             self._rows = params[0]
             self._columns = params[1]
             self._cells = params[2]
@@ -26,11 +26,11 @@ class Game:
                 self._cells[i].append(Cell(i, j, rnd.choice(list_of_enums)))
 
     @staticmethod
-    def read_field_from_file(pathToFile: str) -> tuple[int, int, list[list[Cell]]]:
+    def read_field_from_file(path_to_file: str) -> tuple[int, int, list[list[Cell]]]:
         row_size = 0
         col_size = 0
         cell_field = []
-        with open(pathToFile, 'r') as f:
+        with open(path_to_file, 'r') as f:
             for rowIndex, stringRow in enumerate(f):
                 cell_field.append([])
                 cells = stringRow.strip().split('\t')
@@ -40,18 +40,18 @@ class Game:
                     raise ValueError("It is not square field")
                 row_size += 1
                 for col, cell in enumerate(cells):
-                    typeOfCell = None
+                    type_of_cell = None
                     cell = cell.strip()
                     match cell:
                         case 'r':
-                            typeOfCell = TypeOfCell.rectangle
+                            type_of_cell = TypeOfCell.rectangle
                         case 'c':
-                            typeOfCell = TypeOfCell.circle
+                            type_of_cell = TypeOfCell.circle
                         case 't':
-                            typeOfCell = TypeOfCell.triangle
+                            type_of_cell = TypeOfCell.triangle
                         case 's':
-                            typeOfCell = TypeOfCell.star
-                    cell_field[rowIndex].append(Cell(rowIndex, col, typeOfCell))
+                            type_of_cell = TypeOfCell.star
+                    cell_field[rowIndex].append(Cell(rowIndex, col, type_of_cell))
             return row_size, col_size, cell_field
 
     def find_neighbours(self, row: int, col: int) -> set[Cell]:
@@ -62,27 +62,23 @@ class Game:
         cells_to_delete.add(self._cells[row][col])
         while stack:
             cell = stack.pop()
-            typecell = cell.typeOfCell
+            type_cell = cell.typeOfCell
             x = cell.row
             y = cell.col
-            if (x + 1 < self._rows and self._cells[x + 1][y].typeOfCell == typecell
+            if (x + 1 < self._rows and self._cells[x + 1][y].typeOfCell == type_cell
                     and self._cells[x + 1][y] not in cells_to_delete):
-
                 cells_to_delete.add(self._cells[x + 1][y])
                 stack.append(self._cells[x + 1][y])
-            if (x - 1 > 0 and self._cells[x - 1][y].typeOfCell == typecell
+            if (x - 1 >= 0 and self._cells[x - 1][y].typeOfCell == type_cell
                     and self._cells[x - 1][y] not in cells_to_delete):
-
                 cells_to_delete.add(self._cells[x - 1][y])
                 stack.append(self._cells[x - 1][y])
-            if (y + 1 < self._columns and self._cells[x][y + 1].typeOfCell == typecell
+            if (y + 1 < self._columns and self._cells[x][y + 1].typeOfCell == type_cell
                     and self._cells[x][y + 1] not in cells_to_delete):
-
                 cells_to_delete.add(self._cells[x][y + 1])
                 stack.append(self._cells[x][y + 1])
-            if (y - 1 > 0 and self._cells[x][y - 1].typeOfCell == typecell
+            if (y - 1 >= 0 and self._cells[x][y - 1].typeOfCell == type_cell
                     and self._cells[x][y - 1] not in cells_to_delete):
-
                 cells_to_delete.add(self._cells[x][y - 1])
                 stack.append(self._cells[x][y - 1])
 
@@ -100,35 +96,34 @@ class Game:
 
         return deepest_cells
 
-    def move_cells(self, firs_to_move: dict[int, int], isNeedToAdd: bool) -> None:
-        columns_to_shift = []
+    def move_cells(self, firs_to_move: dict[int, int], is_need_to_add: bool) -> None:
+        first_empty_col = -1
         for col, row in firs_to_move.items():
-            count_of_None = 0
+            count_of_none = 0
             for curIndex in range(row, -1, -1):
                 if self._cells[curIndex][col] is None:
-                    count_of_None += 1
+                    count_of_none += 1
                     continue
-                self._cells[curIndex + count_of_None][col] = self._cells[curIndex][col]
+                self._cells[curIndex + count_of_none][col] = self._cells[curIndex][col]
                 self._cells[curIndex][col] = None
-            if isNeedToAdd:
+            if is_need_to_add:
                 list_of_types = list(TypeOfCell)
-                for i in range(count_of_None):
+                for i in range(count_of_none):
                     self._cells[i][col] = Cell(i, col, rnd.choice(list_of_types))
-            elif count_of_None == self._rows:
-                columns_to_shift.append(col)
+            elif count_of_none == self._rows:
+                first_empty_col = max(col, first_empty_col)
+        if first_empty_col != -1:
+            self.move_empty_column(first_empty_col)
 
-        if columns_to_shift:
-            columns_to_shift.sort()
-            self.move_empty_column(columns_to_shift)
-
-    def move_empty_column(self, columns_to_shift: list) -> None:
-        for curCol in columns_to_shift:
-            for i in range(curCol + 1, self._columns):
-                if self._cells[self._rows - 1][i] is None:
-                    continue
-                for rowIndexToCopy in range(self._rows):
-                    self._cells[rowIndexToCopy][curCol] = self._cells[rowIndexToCopy][i]
-                    self._cells[rowIndexToCopy][i] = None
+    def move_empty_column(self, left_empty_col) -> None:
+        count_to_shift = 1
+        for i in range(left_empty_col + 1, self._columns):
+            if self._cells[self._rows - 1][i] is None:
+                count_to_shift += 1
+                continue
+            for rowIndexToCopy in range(self._rows):
+                self._cells[rowIndexToCopy][i - count_to_shift] = self._cells[rowIndexToCopy][i]
+                self._cells[rowIndexToCopy][i] = None
 
     def print(self) -> None:
         for i in self._cells:
@@ -144,19 +139,16 @@ class Game:
                     letter = 't'
                 print(letter, "\t", end="")
             print()
+        print('-' * 3 * (len(self._cells[0])))
 
 
-# нужен метод который будет удалять одинаковые клетки и сдвигать те, что находятся над ними.
-
-
+# проверить как работает метод сдвига пустых столбцов
 if __name__ == '__main__':
-    game = Game(pathToFile="testField.txt")
+    game = Game(path_to_file="testField.txt")
+
     game.print()
-    print('--------------------------')
-    to_delete = game.find_neighbours(0, 4)
+    to_delete = game.find_neighbours(1, 3)
     deepest = game.delete_cells(to_delete)
     game.print()
-    print('--------------------------')
-    game.move_cells(deepest, True)
+    game.move_cells(deepest, False)
     game.print()
-    print('--------------------------')
